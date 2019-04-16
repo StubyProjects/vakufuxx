b8fec555d496b7d889bf0939fb2241243eb2e282
StubyProjects <jakob.stuby@web.de>
2019-04-16 22:56:45 +0200

    fixed a bug

--- forms/boot.php
+++ forms/boot.php
@@ -6,21 +6,21 @@
   require 'vendor/autoload.php';
   require 'env.php';
 
-
+/*
 $gateway = new Braintree_Gateway([
     'environment' => 'sandbox',
-    'merchantId' => getenv('MERCHANT_ID_PD'),
-    'publicKey' => getenv('PUBLIC_KEY_PD'),
-    'privateKey' => getenv('PRIVATE_KEY_PD')
+    'merchantId' => getenv('MERCHANT_ID_SB'),
+    'publicKey' => getenv('PUBLIC_KEY_SB'),
+    'privateKey' => getenv('PRIVATE_KEY_SB')
 ]);
+*/
 
-/*
   $gateway = new Braintree_Gateway([
       'environment' => 'production',
       'merchantId' => getenv('MERCHANT_ID_PD'),
       'publicKey' => getenv('PUBLIC_KEY_PD'),
       'privateKey' => getenv('PRIVATE_KEY_PD')
   ]);
-*/
+
 
 ?>
